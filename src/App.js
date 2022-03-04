a378165bd2c3716d18f653d6fa6c70d09263215b
Jade Asmus <jadeasmus99@gmail.com>
2022-03-03 21:42:41 -0800

    basic header and hero

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as Logo } from './full_logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        {/* left */}
+        <div className="logo">
+          < Logo className="logo-svg"/>
+        </div>
+
+        {/* right */}
+        <div className="pagination">
+          <span>
+            <a className="pagination-link">
+              Home
+            </a>
+            <a className="pagination-link">
+              About
+            </a>
+            <a className="pagination-link-nr">
+              New Route
+            </a>
+          </span>
+        </div>
       </header>
+
+      <div className="map-container">
+        <h1 className="text">COMING SOON</h1>
+      </div>
+
+
     </div>
   );
 }
